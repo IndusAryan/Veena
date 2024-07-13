966f0d5b9e66ea88399b71431e58d09a6024add9
IndusAryan <serena01011975@gmail.com>
2024-07-13 18:55:07 +0530

    set path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,8 @@ android {
 
         signingConfigs {
             create("release") {
+                storeFile = file("/home/runner/work/Veena/Veena/keystore.jks") ?:
+                        file("/home/runner/work/Veena/Veena/app/keystore.jks")
                 storePassword = System.getenv("KEYSTORE_PASSWORD")
                 keyAlias = System.getenv("KEY_ALIAS")
                 keyPassword = System.getenv("KEY_PASSWORD")
